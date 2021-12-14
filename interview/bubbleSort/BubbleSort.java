f6513ef994a5f3b3ec77513e98810495edef7dd0
sundaym <sunday2011s@gmail.com>
2021-12-14 16:24:36 +0800

    冒泡

--- interview/bubbleSort/BubbleSort.java
+++ interview/bubbleSort/BubbleSort.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
  */
 public class BubbleSort {
     public static int[] bubbleSort(int[] arr) {
+        // n - 1个元素排序, 第n个自动成序
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
